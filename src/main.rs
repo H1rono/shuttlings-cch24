505fa2f26ab3f42f62e2e49a9712642251208ed9
H1rono <hronok66@gmail.com>
2024-12-02 23:14:12 +0900

    :adhesive_bandage: Rename main fn

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use warp::Reply;
 mod routes;
 
 #[shuttle_runtime::main]
-async fn warp() -> shuttle_warp::ShuttleWarp<(impl Reply,)> {
+async fn main() -> shuttle_warp::ShuttleWarp<(impl Reply,)> {
     let route = routes::make();
     Ok(route.boxed().into())
 }
