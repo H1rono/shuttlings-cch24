8e2eebe01834504137c291967a4a58fdee03ee3f
H1rono <hronok66@gmail.com>
2024-12-07 02:13:37 +0900

    :sparkles: Load `MANIFEST_KEYWORD`

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,13 @@ async fn main(
     #[shuttle_runtime::Secrets] secrets: shuttle_runtime::SecretStore,
 ) -> shuttle_warp::ShuttleWarp<(impl Reply,)> {
     let seek_url = secrets.get("SEEK_URL").context("secret SEEK_URL not set")?;
-    let state = routes::State::builder().seek_url(seek_url).build()?;
+    let manifest_keyword = secrets
+        .get("MANIFEST_KEYWORD")
+        .context("secret MANIFEST_KEYWORD not set")?;
+    let state = routes::State::builder()
+        .seek_url(seek_url)
+        .manifest_keyword(manifest_keyword)
+        .build()?;
     let route = routes::make(state);
     Ok(route.boxed().into())
 }
