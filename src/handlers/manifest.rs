d67e5895cf84bfd9a35d66ba59b551ccd47aaf79
H1rono <hronok66@gmail.com>
2024-12-07 02:11:19 +0900

    :sparkles: Add `handlers::manifest::State`

--- src/handlers/manifest.rs
+++ src/handlers/manifest.rs
@@ -1,6 +1,53 @@
+use std::borrow::Cow;
 use std::fmt;
 
+use anyhow::Context;
 use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize)]
+pub struct State {
+    pub(super) manifest_keyword: String,
+}
+
+impl State {
+    pub fn builder() -> Builder {
+        Builder::new()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Default, Hash, Deserialize, Serialize)]
+pub struct Builder {
+    manifest_keyword: Option<String>,
+}
+
+#[allow(dead_code)]
+impl Builder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn set_manifest_keyword(self, value: Option<String>) -> Self {
+        Self {
+            manifest_keyword: value,
+        }
+    }
+
+    pub fn manifest_keyword<'s, S>(self, value: S) -> Self
+    where
+        S: Into<Cow<'s, str>>,
+    {
+        let manifest_keyword = value.into().into_owned();
+        Self {
+            manifest_keyword: Some(manifest_keyword),
+        }
+    }
+
+    pub fn build(self) -> anyhow::Result<State> {
+        let manifest_keyword = self.manifest_keyword.context("manifest_keyword not set")?;
+        let state = State { manifest_keyword };
+        Ok(state)
+    }
+}
 
 pub type Manifest = cargo_manifest::Manifest<PackageMetadata>;
 
