f69f222c6a08c98c23cb4d2a9b99caa3c99a1bb0
H1rono <hronok66@gmail.com>
2024-12-17 11:05:44 +0900

    :recycle: Add `get_secret!` macro

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,25 @@ use warp::Reply;
 
 use shuttlings_cch24 as lib;
 
+macro_rules! get_secret {
+    ($s:ident.$k:ident) => {
+        $s.get(stringify!($k))
+            .context(concat!("secret ", stringify!($k), " not set"))
+    };
+}
+
 #[shuttle_runtime::main]
 async fn main(
     #[shuttle_runtime::Secrets] secrets: shuttle_runtime::SecretStore,
 ) -> shuttle_warp::ShuttleWarp<(impl Reply,)> {
     let env_filter = EnvFilter::try_from_default_env()
         .context("from env failed")
-        .or_else(|_| {
-            secrets
-                .get("CCH24_LOG")
-                .context("secret CCH24_LOG not set")
-                .map(EnvFilter::from)
-        })
+        .or_else(|_| get_secret!(secrets.CCH24_LOG).map(EnvFilter::from))
         .unwrap_or_else(|_| "info".into());
     tracing_subscriber::fmt().with_env_filter(env_filter).init();
 
-    let seek_url = secrets.get("SEEK_URL").context("secret SEEK_URL not set")?;
-    let manifest_keyword = secrets
-        .get("MANIFEST_KEYWORD")
-        .context("secret MANIFEST_KEYWORD not set")?;
+    let seek_url = get_secret!(secrets.SEEK_URL)?;
+    let manifest_keyword = get_secret!(secrets.MANIFEST_KEYWORD)?;
     let jwt_manager = load_jwt_manager(&secrets)?;
     let cookie_manager = load_cookie_manager(&secrets)?;
     let state = lib::routes::State::builder()
@@ -42,15 +42,11 @@ async fn main(
 
 #[tracing::instrument(skip_all)]
 fn load_jwt_manager(secrets: &shuttle_runtime::SecretStore) -> anyhow::Result<lib::jwt::Manager> {
-    let issuer = secrets
-        .get("JWT_ISSUER")
-        .context("secret JWT_ISSUER not set")
+    let issuer = get_secret!(secrets.JWT_ISSUER)
         .inspect_err(|e| tracing::error!(%e))
         .unwrap_or_else(|_| env!("CARGO_PKG_NAME").to_string());
-    let key = secrets.get("JWT_KEY").context("secret JWT_KEY not set")?;
-    let expires_in: i64 = secrets
-        .get("JWT_EXPIRES_IN")
-        .context("secret JWT_EXPIRES_IN not set")
+    let key = get_secret!(secrets.JWT_KEY)?;
+    let expires_in: i64 = get_secret!(secrets.JWT_EXPIRES_IN)
         .inspect_err(|e| tracing::error!(%e))
         .unwrap_or_else(|_| "86400".to_string()) // 1 day in seconds
         .parse()?;
@@ -67,12 +63,8 @@ fn load_jwt_manager(secrets: &shuttle_runtime::SecretStore) -> anyhow::Result<li
 fn load_cookie_manager(
     secrets: &shuttle_runtime::SecretStore,
 ) -> anyhow::Result<lib::cookie::Manager> {
-    let name = secrets
-        .get("COOKIE_NAME")
-        .context("secret COOKIE_NAME not set")?;
-    // let max_age: i64 = secrets
-    //     .get("COOKIE_MAX_AGE")
-    //     .context("secret COOKIE_MAX_AGE not set")
+    let name = get_secret!(secrets.COOKIE_NAME)?;
+    // let max_age: i64 = get_secret!(secrets.COOKIE_MAX_AGE)
     //     .inspect_err(|e| tracing::error!(%e))
     //     .unwrap_or_else(|_| "86400".to_string())
     //     .parse()?;
